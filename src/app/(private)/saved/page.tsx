b19f81c18c2a3690c8baf4da075bcda5de5d0278
manasi sadhu <ms.manasisadhu@gmail.com>
2026-07-12 00:48:37 +0530

    feat: add metadata to saved route

--- src/app/(private)/saved/page.tsx
+++ src/app/(private)/saved/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Saved Posts | PixSlash",
+  description:
+    "Access all your saved wallpapers in one place and revisit your favorite posts on PixSlash.",
+};
+
 const page = () => {
   return <div>Saved Post</div>;
 };
