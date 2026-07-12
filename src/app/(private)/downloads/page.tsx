b9ed9d6f1dc5041d5d8d9a7d1e92fb3cf69c9d04
manasi sadhu <ms.manasisadhu@gmail.com>
2026-07-12 08:57:36 +0530

    feat: add metadata on  likes, downloads,  category route

--- src/app/(private)/downloads/page.tsx
+++ src/app/(private)/downloads/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Downloaded Wallpapers | PixSlash",
+  description:
+    "View all the wallpapers you've downloaded from PixSlash. Quickly access and revisit your download history.",
+};
+
 const page = () => {
   return (
     <div className="grid h-dvh place-items-center">
--- src/app/(public)/category/page.tsx
+++ src/app/(public)/category/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Categories | PixSlash",
+  description:
+    "Browse all wallpaper categories on PixSlash. Discover high-quality wallpapers organized by themes, styles, and interests.",
+};
+
 const page = () => {
   return <div className="grid h-dvh place-items-center">All Categories</div>;
 };
