be23ce82f03ef0eb1f0e2a4d339e1955bddc1b47
manasi sadhu <ms.manasisadhu@gmail.com>
2026-07-12 09:03:43 +0530

    fix: correct typo in homepage metadata description

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Stunning Free Wallpapers & Images | PixSlash",
   description:
-    "The best free stock photos, royalty free images & Wallpapers shared by creators. Discover, collect stunning wallpapers. Pixslash is your destination for high-quality wallpapers.",
+    "The best free stock photos, royalty free images & Wallpapers shared by creators. Discover, collect stunning wallpapers. PixSlash  is your destination for high-quality wallpapers.",
 };
 
 const page = () => {
