c94d0329770ff64a5808cf8fd70cac3c687fbb13
manasi sadhu <ms.manasisadhu@gmail.com>
2026-07-12 08:45:45 +0530

    update: metadata on login page

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Login | PixSlash",
   description:
-    "Log in in to your PixSlash account to explore, upload, organize, and download high-quality wallpapers for desktop and mobile devices.",
+    "Log in to your PixSlash account to explore, upload, organize, and download high-quality wallpapers for desktop and mobile devices.",
 };
 
 const page = () => {
